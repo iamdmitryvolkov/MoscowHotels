571f4a65da88b6a88c1a7aad73942be2a2fcfadf
dmitry <mrdaemonsmile@gmail.com>
2017-08-03 16:37:54 +0300

    Translated to kotlin

---
+++ app/src/main/kotlin/ru/indefinitedream/moscowhotels/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.indefinitedream.moscowhotels
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Main Activity class
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
